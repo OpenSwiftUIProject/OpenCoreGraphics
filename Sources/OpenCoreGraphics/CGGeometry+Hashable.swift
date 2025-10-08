706d964f419ce293fe1c7afa9b784de0d91f7e9a
Kyle <kyle201817146@gmail.com>
2025-10-09 03:01:33 +0800

    Fix missing import

--- Sources/OpenCoreGraphics/CGGeometry+Hashable.swift
+++ Sources/OpenCoreGraphics/CGGeometry+Hashable.swift
@@ -5,21 +5,23 @@
 #if !canImport(Darwin)
 // Foundation does not provide Hashable conformance for CG types yet.
 // See https://github.com/swiftlang/swift-corelibs-foundation/issues/5275
-extension CGPoint: Hashable {
+public import Foundation
+
+extension CGPoint: Swift.Hashable {
     public func hash(into hasher: inout Hasher) {
         hasher.combine(x)
         hasher.combine(y)
     }
 }
 
-extension CGSize: Hashable {
+extension CGSize: Swift.Hashable {
     public func hash(into hasher: inout Hasher) {
         hasher.combine(width)
         hasher.combine(height)
     }
 }
 
-extension CGRect: Hashable {
+extension CGRect: Swift.Hashable {
     public func hash(into hasher: inout Hasher) {
         hasher.combine(origin)
         hasher.combine(size)
