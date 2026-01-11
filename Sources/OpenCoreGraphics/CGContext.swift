02ae0d558852d18219aab3d364fba7ba030f9e91
Kyle <kyle201817146@gmail.com>
2026-01-12 04:35:35 +0800

    Fix Hashable issue

--- Sources/OpenCoreGraphics/CGContext.swift
+++ Sources/OpenCoreGraphics/CGContext.swift
@@ -5,4 +5,12 @@
 //  Created by Kyle on 1/11/26.
 //  Status: Empty
 
-open class CGContext {}
+public class CGContext: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
+
+    public static func == (lhs: CGContext, rhs: CGContext) -> Bool {
+        lhs === rhs
+    }
+}
--- Sources/OpenCoreGraphics/CGImage.swift
+++ Sources/OpenCoreGraphics/CGImage.swift
@@ -4,4 +4,14 @@
 //
 //  Created by Kyle on 1/11/26.
 
-open class CGImage {}
+public class CGImage: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
+
+    public static func == (lhs: CGImage, rhs: CGImage) -> Bool {
+        lhs === rhs
+    }
+}
+
+extension CGImage: @unchecked Sendable {}
